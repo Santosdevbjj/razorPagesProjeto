0bb2a4f590914530b3a34d12ba46b13fc4f17370
Sergio <santossergiorealbjj@outlook.com>
2025-10-30 14:54:32 -0300

    Create Index.cshtml.cs

---
+++ Pages/Contatos/Index.cshtml.cs
@@ -0,0 +1,16 @@
+public class IndexModel : PageModel
+{
+    private readonly IContatoRepository _repo;
+
+    public IndexModel(IContatoRepository repo)
+    {
+        _repo = repo;
+    }
+
+    public List<Contato> Contatos { get; set; } = new();
+
+    public async Task OnGetAsync()
+    {
+        Contatos = await _repo.GetAllAsync();
+    }
+}
